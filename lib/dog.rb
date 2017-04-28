ad387555def4a575570b1153ffd5145d3e7e7cc2
terrancekoar <terrancekoar@gmail.com>
2017-04-28 09:03:17 -0400

    done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each do |puppy|
+      puts puppy.name
+    end
+  end
+
+end
